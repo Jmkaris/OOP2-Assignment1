1fe214e22137e040a19e01e8efcfde5e812b66ba
Jmkaris <mugwimikariuki101@gmail.com>
2025-02-02 11:05:57 +0300

    Update and rename assignment.java to hello world.java

---
+++ hello world.java
@@ -0,0 +1,10 @@
+Question 1- printing Hello world
+  
+  //CT101/G/19500/23
+//This a code dhowing the simple syntax for printing hello world
+public class Main{
+    public static void main(String[] args){
+        System.out.println("Hello world");
+    }
+}
+
